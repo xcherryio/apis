3e503c3010ff2bd25530e68e09b35ec81ca5cf0a
Quanzheng Long <qlong@indeed.com>
2023-11-02 20:55:01 -0700

    Add missing 424 status code to start process (#17)

--- goapi/xdbapi/api_default.go
+++ goapi/xdbapi/api_default.go
@@ -469,6 +469,17 @@ func (a *DefaultAPIService) ApiV1XdbServiceProcessExecutionStartPostExecute(r Ap
 			}
 			newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
 			newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
+		if localVarHTTPResponse.StatusCode == 424 {
+			var v ApiErrorResponse
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
+			newErr.model = v
 		}
 		return localVarReturnValue, localVarHTTPResponse, newErr
 	}
